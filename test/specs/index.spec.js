c0671a7913bcf853581175eb1502be7f6627b999
xlaoyu <frank_yll@163.com>
2018-03-07 00:49:10 +0800

    test: 完善测试

--- test/specs/index.spec.js
+++ test/specs/index.spec.js
@@ -9,7 +9,7 @@ describe('enhanced-el-tree', () => {
   const getTreeVm = (props, options) => {
     return createVue(Object.assign({
       template: `
-        <el-tree ref="tree" :data="data" horizontal ${ props }></el-tree>
+        <enhanced-el-tree ref="tree" node-key="id" :data="data" horizontal ${ props }></enhanced-el-tree>
         `,
 
       data() {
@@ -37,8 +37,19 @@ describe('enhanced-el-tree', () => {
               label: '二级 2-1'
             }, {
               id: 22,
-              label: '二级 2-2'
-            }]
+              label: '二级 2-2',
+              children: [
+                {
+                  id: 221,
+                  label: '二级 2-1-1'
+                }
+              ]
+            },
+            {
+              id: 23,
+              label: '二级 2-3'
+            }
+          ]
           }, {
             id: 3,
             label: '一级 3',
@@ -62,7 +73,19 @@ describe('enhanced-el-tree', () => {
   it('horizontal tree', () => {
     vm = getTreeVm();
     expect(document.querySelector('.el-tree')).to.exist;
-    expect(document.querySelectorAll('.el-tree > .el-tree-node').length).to.equal(3);
+    expect(document.querySelector('.el-tree > .el-tree-node-wrapper')).to.exist;
+    expect(document.querySelectorAll('.el-tree .el-tree-node-block > .el-tree-node').length).to.equal(3);
+  });
 
+  it('new functions', () => {
+    vm = getTreeVm();
+    const checked = [1, 11, 111, 2, 21, 22, 221, 32];
+    vm.$refs.tree.setCheckedKeysPrecisely(checked);
+    vm.$nextTick(_ => {
+      expect(vm.$refs.tree.getNode(2).indeterminate).to.true;
+      expect(vm.$refs.tree.getNode(22).checked).to.true;
+      expect(vm.$refs.tree.getAllCheckedKeys().sort().join() === checked.sort().join()).to.true;
+      expect(vm.$refs.tree.getCheckedKeysWithoutChild().sort().join() === [1, 21, 22, 32].sort().join()).to.true;
+    })
   });
 });
